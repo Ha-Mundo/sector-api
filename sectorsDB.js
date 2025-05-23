72533c431422a3ea17c4ce0def357d600cded973
Ha-Mundo <trauzzu@gmail.com>
2025-05-23 17:24:47 +0200

    moved static data to a separate file

---
+++ sectorsDB.js
@@ -0,0 +1,41 @@
+// Static sector data
+const sectorsDB = [
+  { date: "2021-06-22", name: "Materials", sector_id: "1" },
+  { date: "2022-11-15", name: "Materials", sector_id: "1" },
+  { date: "2023-03-05", name: "Materials", sector_id: "1" },
+
+  { date: "2022-01-10", name: "Industrials", sector_id: "2" },
+  { date: "2024-04-22", name: "Industrials", sector_id: "2" },
+  { date: "2025-05-01", name: "Industrials", sector_id: "2" },
+
+  { date: "2023-04-18", name: "Real Estate", sector_id: "3" },
+  { date: "2021-07-30", name: "Real Estate", sector_id: "3" },
+
+  { date: "2025-02-09", name: "Energy", sector_id: "4" },
+  { date: "2024-06-17", name: "Energy", sector_id: "4" },
+  { date: "2022-09-03", name: "Energy", sector_id: "4" },
+
+  { date: "2024-11-05", name: "Communication Services", sector_id: "5" },
+  { date: "2023-12-25", name: "Communication Services", sector_id: "5" },
+
+  { date: "2021-09-28", name: "Healthcare", sector_id: "6" },
+  { date: "2022-10-10", name: "Healthcare", sector_id: "6" },
+  { date: "2025-01-30", name: "Healthcare", sector_id: "6" },
+
+  { date: "2020-12-31", name: "Information Technology", sector_id: "7" },
+  { date: "2023-08-09", name: "Information Technology", sector_id: "7" },
+
+  { date: "2023-07-13", name: "Consumer Staples", sector_id: "8" },
+  { date: "2022-03-16", name: "Consumer Staples", sector_id: "8" },
+
+  { date: "2025-01-03", name: "Financials", sector_id: "9" },
+  { date: "2024-02-28", name: "Financials", sector_id: "9" },
+
+  { date: "2022-05-27", name: "Utilities", sector_id: "10" },
+  { date: "2023-11-07", name: "Utilities", sector_id: "10" },
+
+  { date: "2024-08-14", name: "Consumer Discretionary", sector_id: "11" },
+  { date: "2021-04-12", name: "Consumer Discretionary", sector_id: "11" }
+];
+
+module.exports = sectorsDB;
