22191c9f8160d0ae9cf400d1141bac5b869d1370
Ha-Mundo <trauzzu@gmail.com>
2025-05-23 17:39:06 +0200

    db data updated

--- sectorsDB.js
+++ sectorsDB.js
@@ -1,41 +1,73 @@
-// Static sector data
 const sectorsDB = [
+  // Materials (1)
   { date: "2021-06-22", name: "Materials", sector_id: "1" },
   { date: "2022-11-15", name: "Materials", sector_id: "1" },
   { date: "2023-03-05", name: "Materials", sector_id: "1" },
+  { date: "2024-08-01", name: "Materials", sector_id: "1" },
+  { date: "2024-08-07", name: "Materials", sector_id: "1" },
 
+  // Industrials (2)
   { date: "2022-01-10", name: "Industrials", sector_id: "2" },
   { date: "2024-04-22", name: "Industrials", sector_id: "2" },
   { date: "2025-05-01", name: "Industrials", sector_id: "2" },
+  { date: "2023-02-13", name: "Industrials", sector_id: "2" },
 
+  // Real Estate (3)
   { date: "2023-04-18", name: "Real Estate", sector_id: "3" },
   { date: "2021-07-30", name: "Real Estate", sector_id: "3" },
+  { date: "2022-10-22", name: "Real Estate", sector_id: "3" },
 
+  // Energy (4)
   { date: "2025-02-09", name: "Energy", sector_id: "4" },
   { date: "2024-06-17", name: "Energy", sector_id: "4" },
   { date: "2022-09-03", name: "Energy", sector_id: "4" },
+  { date: "2023-12-01", name: "Energy", sector_id: "4" },
+  { date: "2021-01-25", name: "Energy", sector_id: "4" },
 
+  // Communication Services (5)
   { date: "2024-11-05", name: "Communication Services", sector_id: "5" },
   { date: "2023-12-25", name: "Communication Services", sector_id: "5" },
+  { date: "2022-02-18", name: "Communication Services", sector_id: "5" },
+  { date: "2022-03-18", name: "Communication Services", sector_id: "5" },
 
+  // Healthcare (6)
   { date: "2021-09-28", name: "Healthcare", sector_id: "6" },
   { date: "2022-10-10", name: "Healthcare", sector_id: "6" },
   { date: "2025-01-30", name: "Healthcare", sector_id: "6" },
+  { date: "2023-06-21", name: "Healthcare", sector_id: "6" },
+  { date: "2023-08-21", name: "Healthcare", sector_id: "6" },
 
+  // Information Technology (7)
   { date: "2020-12-31", name: "Information Technology", sector_id: "7" },
   { date: "2023-08-09", name: "Information Technology", sector_id: "7" },
+  { date: "2022-06-14", name: "Information Technology", sector_id: "7" },
+  { date: "2021-09-05", name: "Information Technology", sector_id: "7" },
+  { date: "2021-09-15", name: "Information Technology", sector_id: "7" },
+  { date: "2021-11-07", name: "Information Technology", sector_id: "7" },
+  { date: "2021-11-14", name: "Information Technology", sector_id: "7" },
+  { date: "2021-11-20", name: "Information Technology", sector_id: "7" },
+  { date: "2021-02-20", name: "Information Technology", sector_id: "7" },
+  { date: "2021-02-22", name: "Information Technology", sector_id: "7" },
+  { date: "2021-01-20", name: "Information Technology", sector_id: "7" },
+  { date: "2021-01-24", name: "Information Technology", sector_id: "7" },
 
+  // Consumer Staples (8)
   { date: "2023-07-13", name: "Consumer Staples", sector_id: "8" },
   { date: "2022-03-16", name: "Consumer Staples", sector_id: "8" },
 
+  // Financials (9)
   { date: "2025-01-03", name: "Financials", sector_id: "9" },
   { date: "2024-02-28", name: "Financials", sector_id: "9" },
+  { date: "2023-11-19", name: "Financials", sector_id: "9" },
 
+  // Utilities (10)
   { date: "2022-05-27", name: "Utilities", sector_id: "10" },
   { date: "2023-11-07", name: "Utilities", sector_id: "10" },
+  { date: "2024-04-30", name: "Utilities", sector_id: "10" },
+  { date: "2021-08-12", name: "Utilities", sector_id: "10" },
 
+  // Consumer Discretionary (11)
   { date: "2024-08-14", name: "Consumer Discretionary", sector_id: "11" },
-  { date: "2021-04-12", name: "Consumer Discretionary", sector_id: "11" }
 ];
 
 module.exports = sectorsDB;
